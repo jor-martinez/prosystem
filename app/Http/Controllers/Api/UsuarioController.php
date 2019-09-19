04ef0ccabe733974ba25cc664e1763f83a1e24d8
Jorge <jor.martinez.salgado@gmail.com>
2019-09-19 10:58:03 -0500

    corrección de errores front

--- app/Http/Controllers/Api/UsuarioController.php
+++ app/Http/Controllers/Api/UsuarioController.php
@@ -14,7 +14,7 @@ class UsuarioController extends Controller
 
     public function index()
     {
-        $usuarios = Usuario::all();
+        $usuarios = Usuario::where('email', '<>', 'admin@prosystem.mx') -> get() ;
         return $usuarios;
     }
 
