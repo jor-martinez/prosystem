c55020ce3eeb1371ba16541d99583c470f0ee02c
DanielBaMe <1fbaesmeneses@gmail.com>
2019-10-04 13:29:53 -0500

    eliminacion de vista en categorias

--- app/Http/Controllers/Api/DescripcionServController.php
+++ app/Http/Controllers/Api/DescripcionServController.php
@@ -14,7 +14,7 @@ class DescripcionServController extends Controller
     public function index()
     {
         $cat = CatServicios::all();
-        return view('dev.create-catServ', ['cat' => $cat]);
+        return $cat;
     }
 
     public function store(Request $request)
@@ -22,8 +22,12 @@ class DescripcionServController extends Controller
         $datos = [
             'titulo' => 'required|string|min:5',
             'descripcion' => 'required|string|min:5',
+            //'imagen' => 'required|image|mimes:jpg,jpeg,png'
         ];
         $this -> validate($request, $datos);
+
+        // $file = $request -> file('imagen');
+        // $nombre_im = $file -> uniqid();
 
         $dato = new CatServicios;
     
@@ -31,7 +35,11 @@ class DescripcionServController extends Controller
         $dato -> titulo = $request -> titulo;
         $dato -> slug = StringReplace::getPureString($request -> titulo);
         $dato -> descripcion = $request -> descripcion;
+        //$dato -> imagen = $nombre_imagen;
         $dato -> save();
+
+        
+        //$upload = Storage::disk('uploads') -> put('images/categorias/' . $nombre_imagen, file_get_contents($file));
 
         return response("creado", 200) -> header('Content-Type', 'application/json');
     }
@@ -45,6 +53,45 @@ class DescripcionServController extends Controller
 
     public function update(Request $request, $id)
     {
+
+        // $datos = [
+        //     'titulo' => 'required|string|min:5',
+        //     'descripcion' => 'required|string|min:10',
+        //     'imagen' => 'required|image|mimes:jpg,jpeg,png'
+        // ];
+        // $this -> validate($request, $datos);
+
+        // $dato = CatServicios::findOrFail($id);
+
+        // if(is_null($request -> file('imagen'))){
+        //     $dato -> titulo = $request -> titulo;
+        //     $dato -> slug = StringReplace::getPureString($request -> titulo);
+        //     $dato -> descripcion = $request -> descripcion;
+        //     $dato -> link = $request -> link;
+        //     $dato -> save();
+    
+        //     return response("actualizado", 200) -> header('Content-Type', 'application/json');
+        // } else {
+        //     $ruta_acceso_imagen = public_path('images/categorias').'/'.$dato -> imagen;
+        //     unlink($ruta_acceso_imagen);
+
+        //     $file = $request -> file('imagen');
+        //     $nombre_imagen = uniqid();
+
+        //     $upload = Storage::disk('uploads') -> put('images/categorias/' . $nombre_imagen, file_get_contents($file));
+        //     $dato -> imagen = $nombre_imagen;
+
+        //     $dato -> titulo = $request -> titulo;
+        //     $dato -> descripcion = $request -> descripcion;
+        //     $dato -> link = $request -> link;
+        //     $dato -> save();
+    
+        //     return response("actualizado", 200) -> header('Content-Type', 'application/json');
+        // }
+
+
+
+
         $datos = [
             'titulo' => 'required|string|min:2',
             'descripcion' => 'required|string|min:2',
@@ -63,7 +110,7 @@ class DescripcionServController extends Controller
     public function destroy($id)
     {
         $dato = CatServicios::findOrFail($titulo);
-        //$ruta_acceso_imagen = public_path('images/blog').'/'.$dato -> encabezado;
+        //$ruta_acceso_imagen = public_path('images/categorias').'/'.$dato -> encabezado;
         //unlink($ruta_acceso_imagen);
         $dato -> delete();
 
